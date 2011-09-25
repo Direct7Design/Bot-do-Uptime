dff5a4c72d82c403b692c5f0b8e061d66b7c6f89
Pedro Felipe <pedro@pedrofelipe.com.br>
2011-09-25 14:08:29 -0300

    1.1 # Adição de recursos
    1. Escolha se o Bot do Uptime irá tweetar, enviar DM ou os 2 e o
    usuário que receberá;
    2. O Bot do Uptime exibe se enviou o tweet/DM ou não, caso haja um erro.

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
-require_once($_SERVER['DOCUMENT_ROOT'].'/twitteroauth.php');
-require_once($_SERVER['DOCUMENT_ROOT'].'/oAuth.php');
+require_once($_SERVER["DOCUMENT_ROOT"]."/twitteroauth.php");
+//require_once($_SERVER["DOCUMENT_ROOT"]."/oAuth.php");
 
-// Registre o aplicativo em https://dev.twitter.com/apps/new e insira as chaves correspondentes nos campos abaixo.
 $consumerKey = 'consumer-key';
 $consumerSecret = 'consumer-secret';
 $oAuthToken = 'oauth-token';
@@ -14,6 +13,13 @@ $tweet = new TwitterOAuth($consumerKey, $consumerSecret, $oAuthToken, $oAuthSecr
 $nome = "Bot do Uptime";
 $url = "http://www.meusite.com.br";
 
+// Você deseja que um tweet e/ou DM seja(m) enviado(s) caso o servidor monitorado esteja ativo ou fora do ar? 0 = Não e 1 = Sim
+$tweetar = 1;
+$dm = 1;
+
+// Caso $dm = 1, preencha o campo abaixo com o usuário que receberá a DM sem o @. Ex.: BotdoUptime
+$userdm = "BotdoUptime";
+
 // 10 é o tempo de espera de resposta. Mude-o se julgar necessário.
 $ch = curl_init($url);  
 	  curl_setopt($ch, CURLOPT_TIMEOUT, 10);  
@@ -23,27 +29,53 @@ $ch = curl_init($url);
 	  $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);  
 	  curl_close($ch);
 
-// Se o CURLINFO_HTTP_CODE retornar um número maior ou igual a 200 e menor que 400, a URL está ativa. Caso contrário, retornará 0, e isto significa que a URL está fora do ar.
+// Se o CURLINFO_HTTP_CODE retornar um número maior ou igual a 200 e menor que 400, o servidor monitorado está ativo. Caso contrário, retornará 0, e isto significa que o servidor monitorado está fora do ar.
 if($httpcode >= 200 && $httpcode < 400){
 
 	// Mensagem que será tweetada e exibida após a execução do arquivo.
 	$mensagem = "O servidor $nome est&aacute; operante! &ndash; C&oacute;digo: $httpcode";
 	
-	// O tweet é enviado.
-	$tweet->post('statuses/update', array('status' => "$mensagem"));
-	
-	// A mensagem é exibida.
-	echo $mensagem;
+	if($tweetar = 1) {
+		// O tweet é enviado se o servidor monitorado estiver ativo. Se desejar, comente as 5 linhas abaixo para que o bot NÃO envie um tweet caso o servidor monitorado esteja ativo.
+		$tweet->post("statuses/update", array("status" => "$mensagem"));
+		if(property_exists($tweet, "error")) {
+  	  		echo "N&atilde;o foi poss&iacute;vel tweetar a seguinte mensagem:<br />$mensagem<br /><br />";
+		} else {
+   			echo "A seguinte mensagem foi enviada para o Twitter:<br />$mensagem<br /><br />";
+   		}
+   	}
+   	
+    if($dm = 1) {
+    	$tweet->post("direct_messages/new", array("text" => "$mensagem", "screen_name" => "$userdm"));
+    	if(property_exists($tweet, "error") ) {
+  	  		echo "N&atilde;o foi poss&iacute;vel enviar uma DM para $userdm com a seguinte mensagem:<br />$mensagem<br /><br />";
+		} else {
+    		echo "A seguinte mensagem foi enviada por DM para $userdm:<br />$mensagem";
+    	}
+    }
 
 } else {
 
 	// Mensagem que será tweetada e exibida após a execução do arquivo.
 	$mensagem = "Ops, acho que o servidor $nome est&aacute; fora do ar! &ndash; C&oacute;digo: $httpcode";
 	
-	// O tweet é enviado.
-	$tweet->post('statuses/update', array('status' => "$mensagem"));
-	
-	// A mensagem é exibida.
-	echo $mensagem;
+	if($tweetar = 1) {
+		// O tweet é enviado se o servidor monitorado estiver fora do ar. Se desejar, comente as 5 linhas abaixo para que o bot NÃO envie um tweet caso o servidor monitorado esteja fora do ar.
+		$tweet->post("statuses/update", array("status" => "$mensagem"));
+		if(property_exists($tweet, "error") ) {
+  	  		echo "N&atilde;o foi poss&iacute;vel tweetar a seguinte mensagem:<br />$mensagem<br /><br />";
+		} else {
+    		echo "A seguinte mensagem foi enviada para o Twitter:<br />$mensagem<br /><br />";
+    	}
+    }
+    
+    if($dm = 1) {
+    	$tweet->post("direct_messages/new", array("text" => "$mensagem", "screen_name" => "$userdm"));
+    	if(property_exists($tweet, "error") ) {
+  	  		echo "N&atilde;o foi poss&iacute;vel enviar uma DM para $userdm com a seguinte mensagem:<br />$mensagem<br /><br />";
+		} else {
+    		echo "A seguinte mensagem foi enviada por DM para $userdm:<br />$mensagem";
+    	}
+    }
 }
 ?>
